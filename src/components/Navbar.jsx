13d5d369b88aa00ed0a37f3fe07720e9ce3dfffa
Mahir <98490297+Mahir-Mamaliyev@users.noreply.github.com>
2022-08-20 21:10:53 +0400

    learning react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <section className="navbar">
+        <div className="logo"></div>
+        <div className="nav-links"></div>
+        <button className="get-started"></button>
+    </section>
+  )
+}
+
+export default Navbar
